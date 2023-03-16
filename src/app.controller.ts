11855bd0b83ac96cd5cacbaf99ca0c1834a582f1
DDangKongE <eogml5581@gmail.com>
2023-03-16 22:24:02 +0900

    feat: openai API 연동
    chatDavinci003 모델 연동

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('chat-davinci')
+  async chatDavinci003(@Query() query: any): Promise<string> {
+    return await this.appService.chatDavinci003(query);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { Configuration, OpenAIApi } from 'openai';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async chatDavinci003(query): Promise<string> {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: 'Human: ' + query?.text || '내용 없음',
+      temperature: 0.9,
+      max_tokens: 1000,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0.6,
+      stop: [' Human:', ' AI:'],
+    });
+
+    const answer_text = response?.data?.choices[0]?.text || '';
+
+    const answer = answer_text.split(': ', 2)[1];
+
+    return answer;
   }
 }
